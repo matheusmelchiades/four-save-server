3506c7df41446e501414b4ea017e2e8450afa255
Matheus Melchiades <matheusmacielmelchiades@hotmail.com>
2019-10-19 23:41:03 -0300

    migrations and models

--- database/migrations/1503248427885_user.js
+++ database/migrations/1503248427885_user.js
@@ -1,22 +1,31 @@
-'use strict'
-
 /** @type {import('@adonisjs/lucid/src/Schema')} */
-const Schema = use('Schema')
+const Schema = use('Schema');
 
 class UserSchema extends Schema {
-  up () {
-    this.create('users', (table) => {
-      table.increments()
-      table.string('username', 80).notNullable().unique()
-      table.string('email', 254).notNullable().unique()
-      table.string('password', 60).notNullable()
-      table.timestamps()
-    })
+  up() {
+    this.create('users', table => {
+      table.increments();
+      table.string('firstName').notNullable();
+      table.string('lastName').notNullable();
+      table
+        .string('email', 254)
+        .notNullable()
+        .unique();
+      table
+        .string('username', 80)
+        .notNullable()
+        .unique();
+      table.string('password', 60).notNullable();
+      table.float('latitude').notNullable();
+      table.float('longitude').notNullable();
+      table.integer('perfilId');
+      table.timestamps();
+    });
   }
 
-  down () {
-    this.drop('users')
+  down() {
+    this.drop('users');
   }
 }
 
-module.exports = UserSchema
+module.exports = UserSchema;
