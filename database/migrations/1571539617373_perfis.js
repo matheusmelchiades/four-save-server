6721cc0540bd521120f9daa91edc26155396e2fe
Matheus Melchiades <matheusmacielmelchiades@hotmail.com>
2019-10-19 23:52:27 -0300

    add migration perfis

---
+++ database/migrations/1571539617373_perfis.js
@@ -0,0 +1,19 @@
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema');
+
+class PerfisSchema extends Schema {
+  up() {
+    this.create('perfis', table => {
+      table.increments();
+      table.string('type').unique();
+      table.string('description');
+      table.timestamps();
+    });
+  }
+
+  down() {
+    this.drop('perfis');
+  }
+}
+
+module.exports = PerfisSchema;
